dcc38e224c586580073b2b2b77884494aa2f0c78
Andrew Ross <andrew.ross@takeda.com>
2026-01-26 15:50:48 -0600

    tsdown config update

--- apps/wallet/src/test/read.ts
+++ apps/wallet/src/test/read.ts
@@ -2,8 +2,18 @@ import { Fs } from "@d0paminedriven/fs";
 
 const fs = new Fs(process.cwd());
 
-const filesArr = fs
-  .readDir("src", { recursive: true })
-  .filter(o =>  !(o.lastIndexOf(".") === -1))
-  .map(v => `src/${v}`);
-console.log(filesArr);
+const filesArr = <const T extends "src" | "dist" = "src">(distOrSrc: T) => {
+  const filePaths = fs
+    .readDir("src", { recursive: true })
+    .filter(o => !(o.lastIndexOf(".") === -1))
+    .filter(o => !o.startsWith("test"))
+    .map(v => `${distOrSrc}/${v}`);
+  if (distOrSrc === "src") {
+    return filePaths;
+  } else {
+    return filePaths.map(p => p.slice(0, p.lastIndexOf(".")).concat(".d.ts"));
+  }
+};
+if (process.argv[3] === "src" || process.argv[3] === "dist") {
+  console.log(filesArr(process.argv[3]));
+}
